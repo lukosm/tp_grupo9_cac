eda3a5e9579592a6d858f7ea6776a0ad0248cc09
anaorov <anaorov@gmail.com>
2022-05-15 00:54:12 -0300

    Empezando log in

---
+++ scripts/login.js
@@ -0,0 +1,7 @@
+const formulario = document.getElementById("form");
+const userEmail = document.getElementById("email");
+const userPass = document.getElementById("pass");
+
+const alertSuccess = document.getElementById("alertSuccess");
+const alertEmail = document.getElementById("alertEmail");
+const alertPass = document.getElementById("alertPass");
